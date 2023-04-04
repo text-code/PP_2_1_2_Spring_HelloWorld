4b781127c17db51667c9db4665fdfc2bf9c15e48
text-code <dssn2102@gmail.com>
2023-04-04 10:58:20 +0900

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanTwo =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat catTwo = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean.equals(beanTwo));
+        System.out.println(cat.equals(catTwo));
+
+
         System.out.println(bean.getMessage());
     }
 }
